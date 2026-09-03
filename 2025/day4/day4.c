9d4ad09bfa117f993a6d1d129101839a905bdb33
mdantec56 <mdantec56@gmail.com>
2026-09-03 05:54:35 +0200

    day4s2 && no memory leaks

--- 2025/day4/day4.c
+++ 2025/day4/day4.c
@@ -76,12 +76,26 @@ int	ft_get_nb_voisins(char **tab, int ligne, int colonne)
 	return (count);
 }
 
+void	ft_free(char **tab)
+{
+	int	i;
+
+	i = 0;
+	while (tab[i])
+	{
+		free(tab[i]);
+		++i;
+	}
+	free(tab);
+}
+
 int	day4(char *fichier)
 {
 	int	cptrolls;
 	int	ligne;
 	int	colonne;
 	char	**tab;
+	int	cpt; // s2
 
 	tab = ft_subsplit(fichier, "\n");
 	ligne = 0;
@@ -90,14 +104,22 @@ int	day4(char *fichier)
 	while (tab[ligne])
 	{
 		colonne = 0;
+		cpt = 0; // s2
 		while (tab[ligne][colonne])
 		{
 			if (tab[ligne][colonne] == '@' && ft_get_nb_voisins(tab, ligne, colonne) < 4)
+			{
+				tab[ligne][colonne] = '.'; // s2
+				++cpt; // s2
 				++cptrolls;
+			}
 			++colonne;
 		}
 		++ligne;
+		if (cpt) // s2
+			ligne = 0; // s2
 	}
+	ft_free(tab);
 	return (cptrolls);
 }
 
@@ -112,5 +134,6 @@ int	main(int argc, char **argv)
 	}
 	fichier = ft_get_fichier(argv[1]);
 	ft_putnbr(day4(fichier));
+	free(fichier);
 	return (0);
 }
